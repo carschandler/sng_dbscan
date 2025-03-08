50669842804ac84446e647b2b44b853902b849b1
carschandler <92899389+carschandler@users.noreply.github.com>
2025-03-08 09:36:59 -0600

    Initial implementation working

--- scripts/random_clusters.py
+++ scripts/random_clusters.py
@@ -1,4 +1,8 @@
 import numpy as np
+import pandas as pd
+import plotly.express as px
+from plotly.subplots import make_subplots
+from sklearn.cluster import DBSCAN
 from sklearn.datasets import make_blobs
 
 from sng_dbscan.sng_dbscan import SNG_DBSCAN
@@ -7,5 +11,33 @@ x, y = make_blobs()  # type: ignore
 
 assert isinstance(x, np.ndarray)
 
-sng = SNG_DBSCAN(0.05, 0.2, 3)
-sng.fit(x)
+epsilon = 2
+min_pts = 3
+
+n = len(x)
+
+sng = SNG_DBSCAN(sampling_rate=np.log(n) / n, max_dist=epsilon, min_points=min_pts)
+sng_labels = sng.fit(x)
+
+dbscan = DBSCAN(eps=epsilon, min_samples=min_pts)
+dbscan_labels = dbscan.fit_predict(x)
+
+df = pd.DataFrame(
+    x,
+    columns=["x", "y"],
+)
+
+df.loc[:, "true"] = y.astype(str)
+df.loc[:, "sng"] = sng_labels.astype(str)
+df.loc[:, "dbscan"] = dbscan_labels.astype(str)
+
+df = df.melt(
+    id_vars=["x", "y"],
+    value_vars=["true", "sng", "dbscan"],
+    value_name="Label",
+    var_name="Cluster Type",
+)
+
+px.scatter(df, x="x", y="y", color="Label", facet_row="Cluster Type").write_html(
+    "out.html", include_plotlyjs="cdn"
+)
--- src/sng_dbscan/sng_dbscan.py
+++ src/sng_dbscan/sng_dbscan.py
@@ -1,3 +1,4 @@
+import itertools
 from typing import Any
 
 import numpy as np
@@ -19,33 +20,67 @@ class SNG_DBSCAN:
 
     def fit(self, x: NDArray):
         graph = Graph(x)
-        n = len(graph.nodes)
-        indices = np.arange(n)
-        n_sample = int(np.ceil(self.sampling_rate * n))
+        num_nodes = len(graph.nodes)
+        indices = np.arange(num_nodes)
+        n_sample = int(np.ceil(self.sampling_rate * num_nodes))
 
-        for i, node in enumerate(graph.nodes):
-            i_sample = self.rng.choice(indices, n_sample)
+        for i_node, node in enumerate(graph.nodes):
+            i_sample = self.rng.choice(indices, n_sample, replace=False)
             sample = graph.nodes[i_sample]
             norms = np.linalg.norm(node - sample, axis=1)
-            matching_indices = i_sample[norms <= self.max_dist]
-            for j in matching_indices:
-                graph.add_edge(i, j)
+            indices_in_range = i_sample[norms <= self.max_dist]
+            for i_in_range in indices_in_range:
+                graph.add_edge(i_node, int(i_in_range))
 
-            min_points_mask = np.vectorize(lambda x: x >= self.min_points)(graph.edges)
+        nodes_with_min_edges = set(
+            [i for i, edge in enumerate(graph.edges) if len(edge) >= self.min_points]
+        )
+
+        connected_components: list[set[int]] = []
+
+        # Iterate through the nodes of degree >= min_points. If a node in this
+        # list has already been visited, it will be removed.
+        while len(nodes_with_min_edges) > 0:
+            # Start the formation of this component by adding the initial node
+            # as a candidate for addition into the component
+            component_candidates = set()
+            # component_candidates.add(next(iter(nodes_with_min_edges)))
+            component_candidates.add(nodes_with_min_edges.pop())
+            component = set()
+            while len(component_candidates) > 0:
+                candidate = component_candidates.pop()
+                component.add(candidate)
+                valid_edge_nodes = graph.edges[candidate] & nodes_with_min_edges
+                component_candidates.update(valid_edge_nodes)
+                nodes_with_min_edges -= valid_edge_nodes
+            connected_components.append(component)
+
+        clusters = connected_components
+
+        unclustered_nodes = set(range(num_nodes)) - set(itertools.chain(*clusters))
+        for node in unclustered_nodes:
+            node_in_clusters = [
+                any([edge_node in cluster for edge_node in graph.edges[node]])
+                for cluster in clusters
+            ]
+            if any(node_in_clusters):
+                i_cluster = int(
+                    self.rng.choice(np.arange(len(clusters))[node_in_clusters])
+                )
+                clusters[i_cluster].add(node)
+
+        labels = -np.ones(num_nodes, np.int32)
+        for label, cluster in enumerate(clusters):
+            labels[list(cluster)] = label
+
+        return labels
 
 
 class Graph:
     def __init__(self, nodes: NDArray):
         self.nodes = nodes
         # self.edges: set[tuple[int, int]] = set()
-        self.edges: NDArray[np.object_] = np.full(len(nodes), set())
-
-    # def add_edge(self, index_1: int, index_2: int):
-    #     """Add edges in ascending index order to ensure no duplicate edges."""
-    #     if index_1 < index_2:
-    #         self.edges.add((index_1, index_2))
-    #     else:
-    #         self.edges.add((index_2, index_1))
+        self.edges: list[set[int]] = [set() for _ in nodes]
 
     def add_edge(self, index_1: int, index_2: int):
         """Add the nodes to each other's set of connecting nodes."""
